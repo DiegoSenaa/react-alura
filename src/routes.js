182d256390c07d2669df461578bab83d5432c4c8
Diego Sena <diego.senaa@gmail.com>
2019-11-07 22:11:02 -0300

    Aulas 6/11 e 7/11

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFound'
 
 class Roteamento extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <PrivateRoute path="/" component={HomePage} exact />
                 <Route path="/login" component={LoginPage} />
+                <Route component={NotFoundPage} />
             </Switch>
         )
     }
 }
 
 export default Roteamento;
+
+class PrivateRoute extends Component {
+    estaAutenticado = () => {
+        if(localStorage.getItem('TOKEN')){
+            return true
+        } else {
+            return false;
+        }
+    }
+
+    render() {
+        const { component: Component, ...props } = this.props;
+
+        if(this.estaAutenticado()) {
+            return <Component {...props} />
+        } else {
+            return <Redirect to='/login' />
+        }
+    }
+}
